662fdcdaa6ddb056b5501e74c61f5496b8847e2e
BilgeTad <34030699+BilgeTad@users.noreply.github.com>
2018-10-24 16:14:56 +0200

    Aufgabe 2

---
+++ Aufgabe2/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Text;
+
+namespace Aufgabe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ /*      
+           Console.WriteLine("Hello World!");
+            var i = 42;
+            var pi = 3.1415;            
+            var salute = "Hello, World";
+            var variable = 3D; 
+            Console.WriteLine(variable);
+
+//_________________________________________________________-
+           
+            int[] ia = {1, 0, 2, 9, 3, 8, 4, 7, 5, 6};
+            int ergebnis = ia[2] * ia[8] + ia[4];
+            Console.WriteLine(ergebnis);  
+
+            double[] da = new double[3];
+            da[0] = 3.14159265359;
+            da[1] = 2.718281828459;
+            da[2] = 2.97;
+
+            Console.WriteLine(ia.Length);
+            Console.WriteLine(da.Length);
+
+//___________________________________________________________
+
+            string meinString = "Dies ist ein String";
+            string a = "Dies ist ";
+            string b = "ein String";
+            string c = a + b;
+            //string a = "eins";
+            //string b = "zwei";
+            //string c = "eins";
+            bool a_eq_b = (a == b);
+            bool a_eq_c = (a == c);
+            char zeichen = meinString[5];
+           
+            Console.WriteLine(a_eq_c);
+
+//_________________________________________________________
+
+            int zahla = int.Parse(Console.ReadLine());
+            int zahlb = int.Parse(Console.ReadLine());
+
+            if(zahla>zahlb){
+                Console.WriteLine("a ist größer als b");
+            }
+            else{
+                Console.WriteLine("b ist größer als a");
+            }
+
+//__________________________________________________________
+
+            int zahlc = int.Parse(Console.ReadLine());
+            int zahld = int.Parse(Console.ReadLine());
+
+            if(zahlc>3 && zahld==6){
+                Console.WriteLine("Du hast gewonnen");
+            }
+            else{
+                Console.WriteLine("Leider verloren");
+            }
+
+//___________________________________________________________
+            
+            string j = Console.ReadLine();
+            switch (j)
+            {
+            case "eins":
+            Console.WriteLine("Du hast EINS eingegeben");
+            break;
+            case "zwei":
+            Console.WriteLine("ZWEI war Deine Wahl");
+            break;
+            case "drei":
+            Console.WriteLine("Du tipptest eine DREI");
+            break;
+            case "vier":
+            Console.WriteLine("Du tipptest eine VIER");
+            break;
+            default:
+            Console.WriteLine("Die Zahl " + j + " kenne ich nicht");
+            break;
+            }
+
+//_______________________________________________________________
+
+            string k = Console.ReadLine();
+            if(k=="eins"){
+            Console.WriteLine("Du hast EINS eingegeben");
+            }
+            if(k=="zwei"){
+            Console.WriteLine("ZWEI war Deine Wahl");    
+            }
+            if(k=="drei"){
+            Console.WriteLine("Du tipptest eine DREI");   
+            }
+            if(k=="vier"){
+            Console.WriteLine("Du tipptest eine VIER");   
+            }
+            else{
+            Console.WriteLine("Die Zahl " + k + " kenne ich nicht");
+            }
+
+//___________________________________________________
+ 
+            int [] zahlen= {1,2,3,4,5,6,7,8,9,10};
+            int x=0;
+            while (x<10)
+            {
+                Console.WriteLine(zahlen[x].ToString());
+                x++;
+            }
+//___________________________________________________________
+
+            int i = 0; 
+            string[] someStrings = 
+            {
+            "Hier",
+            "sehen",
+            "wir",
+            "einen",
+            "Array",
+            "von",
+            "Strings"
+            };
+  
+            while (i < someStrings.Length)
+            {
+            Console.WriteLine(someStrings[i].ToString());
+            i++;
+            }         
+//__________________________________________________
+ */ 
+            string[] someStrings = 
+            {
+            "Hier",
+            "sehen",
+            "wir",
+            "einen",
+            "Array",
+            "von",
+            "Strings"
+            };
+             /*
+            for (int i = 0; i < someStrings.Length; i++)
+            {
+            Console.WriteLine(someStrings[i]);
+            }
+           
+
+            int i = 0;
+            do
+            {
+            Console.WriteLine(someStrings[i]);
+            i++;
+            }
+            while (i < someStrings.Length);
+            */
+            int i = 0;
+            while (true)
+            {
+            Console.WriteLine(someStrings[i]);
+            if (i > someStrings.Length)
+            break;
+            i++;
+            }
+        }
+    }
+}
