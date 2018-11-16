6e4bbba7f8f46c2d7c6e22188cf8cb78abe05943
BilgeTad <34030699+BilgeTad@users.noreply.github.com>
2018-11-16 22:46:48 +0100

    Aufgabe 5

--- Aufgabe05/Program.cs
+++ Aufgabe05/Program.cs
@@ -8,7 +8,7 @@ namespace Aufgabe05
         {
             Console.WriteLine("Enter Text: ");
 
-            var enteredText = Console.ReadLine();
+            string enteredText = Console.ReadLine();
             string turnedText = TurnAll(enteredText);
             string outputTurnedWord = TurnWord(enteredText);
             string outputText = TurnWordLetter(enteredText);
@@ -57,7 +57,6 @@ namespace Aufgabe05
                 {
                     wordIsTurned += wordToTurn[k];
                 }
-                //wordToTurn="";
                 outputText[i] = wordIsTurned;
 
             }
